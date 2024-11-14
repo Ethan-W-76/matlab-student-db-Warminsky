49540f7d263e52a5a279ddb9375e66115ebde756
Ethan Warminsky <ethan.warminsky@gmail.com>
2024-11-13 20:26:20 -0500

    Update plotAgeDistribution.m

--- plotAgeDistribution.m
+++ plotAgeDistribution.m
@@ -8,7 +8,7 @@
 % Submission Date: [11/13/24]
 % All rights are reserved - GNU License
 
-#Creates a age distrabution histogram
+# Generates an age distribution histogram
 function plotAgeDistribution(students)
     ages = [students.Age];
     histogram(ages, 'FaceColor', 'r');

a6af9b61526b75eff9a04faeb11291e09e363877
Ethan Warminsky <ethan.warminsky@gmail.com>
2024-11-13 20:37:28 -0500

    Update Student.m

--- Student.m
+++ Student.m
@@ -9,7 +9,7 @@
 % All rights are reserved - GNU License
 
 classdef Student
-    # Defines class properties
+    % Defines class properties
     properties
         ID
         Name
@@ -19,7 +19,7 @@ classdef Student
     end
     
     methods
-    # Turns variables into objects
+    % Turns variables into objects
         function obj = Student(id, name, age, gpa, major)
             obj.ID = id;
             obj.Name = name;
@@ -27,12 +27,12 @@ classdef Student
             obj.GPA = gpa;
             obj.Major = major;
         end
-        # Displays objects
+        % Displays objects
         function displayInfo(obj)
             fprintf('ID: %s\nName: %s\nAge: %d\nGPA: %.2f\nMajor: %s\n', ...
                 obj.ID, obj.Name, obj.Age, obj.GPA, obj.Major);
         end
-        # Allows GPA to be updated
+        % Allows GPA to be updated
         function obj = updateGPA(obj, newGPA)
             obj.GPA = newGPA;
         end
